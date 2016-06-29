eadf25c36750f2c386eb4e8624dc8fa7bbcaf6a1
Richard Li <richardli4824@gmail.com>
2016-06-29 16:57:56 -0700

    Added Icons

--- chinaCoders/chinaCoders/Controllers/ManageController.cs
+++ chinaCoders/chinaCoders/Controllers/ManageController.cs
@@ -11,7 +11,7 @@ using chinaCoders.Models;
 namespace chinaCoders.Controllers
 {
     [Authorize]
-    public class ManageController : Controller
+    public class ManageController : BaseController
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
